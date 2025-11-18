2c4ccf19090c0df1c077f5fc4c98070dbf720524
crist <cristhianchulca49@gmail.com>
2025-11-18 12:45:22 +0100

    refactor: rename package for better organization

--- src/main/java/Level1_E1/Main.java
+++ src/main/java/Level1_E1/Main.java
@@ -1,4 +1,4 @@
-package Level1;
+package Level1_E1;
 
 import java.util.ArrayList;
 import java.util.HashSet;
--- src/main/java/Level1_E1/Month.java
+++ src/main/java/Level1_E1/Month.java
@@ -1,4 +1,4 @@
-package Level1;
+package Level1_E1;
 
 public class Month {
     private String name;
