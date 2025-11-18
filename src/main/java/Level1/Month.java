a4256167366c1768e7d9ce3299e05f261977aa57
crist <cristhianchulca49@gmail.com>
2025-11-18 12:36:32 +0100

    feat: override equals() to compare Month objects by name content

--- src/main/java/Level1/Month.java
+++ src/main/java/Level1/Month.java
@@ -10,4 +10,18 @@ public class Month {
     public String getName() {
         return name;
     }
+
+    @Override
+
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Month month = (Month) obj;
+        return this.name.equals(month.name);
+    }
+
 }
