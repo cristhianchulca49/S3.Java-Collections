43fdfc536df8ace104a93a289e6fc092eca6cd56
crist <cristhianchulca49@gmail.com>
2025-11-18 13:27:09 +0100

    feat: create numbersList2 and fill it with numberList1 in reverse order with ListIterator

--- src/main/java/Level1_E2/Main.java
+++ src/main/java/Level1_E2/Main.java
@@ -11,5 +11,17 @@ public class Main {
         for (int i = 1; i <= 50; i++) {
             numbersList1.add(i);
         }
+
+        List<Integer> numbersList2 = new ArrayList<>();
+
+        ListIterator<Integer> it = numbersList1.listIterator(numbersList1.size());
+        while (it.hasPrevious()) {
+            numbersList2.add(it.previous());
+        }
+
+        System.out.println("List 1");
+        System.out.println(numbersList1);
+        System.out.println("List 2");
+        System.out.println(numbersList2);
     }
 }
