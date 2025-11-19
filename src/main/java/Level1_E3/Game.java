df7a9c6f9a250ac6515f334df1eae2eeee1bab21
crist <cristhianchulca49@gmail.com>
2025-11-19 12:47:00 +0100

    feat: add initialGame method to get random country and check user answer

--- src/main/java/Level1_E3/Game.java
+++ src/main/java/Level1_E3/Game.java
@@ -1,10 +1,29 @@
 package Level1_E3;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Game {
     private String name;
     private int score = 0;
 
     public Game(){
         this.name = Input.readString("Please, Enter your name: ");
+    }
+    public void initialGame(HashMap<String, String> countries) {
+        List<String> countriesList = new ArrayList<>(countries.keySet());
+
+        for (int i = 0; i < 10; i++) {
+            int random = (int) (Math.random() * countries.size());
+            String country = countriesList.get(random);
+            String answer = Input.readString("Guess the capital of : " + country);
+            if (answer.equalsIgnoreCase(countries.get(country))) {
+                System.out.println("correct!!");
+                score++;
+            } else {
+                System.out.println("incorrect!! is: " + countries.get(country));
+            }
+        }
     }
 }
