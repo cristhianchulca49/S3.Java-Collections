81fe6a4525a00e68e09cb28134bbafd43f33f6a8
crist <cristhianchulca49@gmail.com>
2025-11-19 10:44:48 +0100

    refactor: move main functionalities into Year class for better encapsulation

--- src/main/java/Level1_E1/Main.java
+++ src/main/java/Level1_E1/Main.java
@@ -7,31 +7,13 @@ import java.util.List;
 
 public class Main {
     static void main(String[] args) {
-        List<Month> year = new ArrayList<>();
+        Year year = new Year();
+        year.printYear();
 
-        year.add(new Month("January"));
-        year.add(new Month("February"));
-        year.add(new Month("March"));
-        year.add(new Month("April"));
-        year.add(new Month("May"));
-        year.add(new Month("June"));
-        year.add(new Month("July"));
-        year.add(new Month("September"));
-        year.add(new Month("October"));
-        year.add(new Month("November"));
-        year.add(new Month("December"));
-        for (Month m : year) {
-            System.out.println(m.getName());
-        }
-        System.out.println("-----------");
+        year.setMonth(7, "August");
+        year.printYear();
 
-        year.add(7, new Month("August"));
-        for (Month m : year) {
-            System.out.println(m.getName());
-        }
-        System.out.println("-----------");
-
-        HashSet<Month> yearHashSet = new HashSet<>(year);
+        HashSet<Month> yearHashSet = new HashSet<>(year.getYear());
         yearHashSet.add(new Month("January"));
 
         Iterator<Month> it = yearHashSet.iterator();
