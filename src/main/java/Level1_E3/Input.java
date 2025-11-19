9c38a7d14cf797f2f596ac55f73d742392b4fe4b
crist <cristhianchulca49@gmail.com>
2025-11-19 11:11:13 +0100

    feat: add readDocument() method to load countries file and return a HashMap with country-capital

--- src/main/java/Level1_E3/Input.java
+++ src/main/java/Level1_E3/Input.java
@@ -1,8 +1,25 @@
 package Level1_E3;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Input {
     private static Scanner sc;
 
+    public static HashMap<String, String> readDocument(String pathname) {
+        HashMap<String, String> mapCountries = new HashMap<>();
+        try {
+            sc = new Scanner(new File(pathname));
+            while (sc.hasNext()) {
+                String country = sc.next();
+                String capital = sc.next();
+                mapCountries.put(country, capital);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+        return mapCountries;
+    }
 }
