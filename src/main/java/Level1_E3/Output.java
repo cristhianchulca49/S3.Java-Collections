835a312d1fbcaee8260b6f383d19a896e95542d8
crist <cristhianchulca49@gmail.com>
2025-11-19 13:00:55 +0100

    feat: add saveScore Method() to save user name and score to a txt file

--- src/main/java/Level1_E3/Output.java
+++ src/main/java/Level1_E3/Output.java
@@ -1,5 +1,18 @@
 package Level1_E3;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Output {
 
+    public static void saveScore(String name, int score) {
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter("scores.txt", true));
+            writer.println(name + ": " + score);
+            System.out.println("Scores saved!");
+        } catch (IOException e) {
+            System.out.println("Error saving score!");
+        }
+    }
 }
