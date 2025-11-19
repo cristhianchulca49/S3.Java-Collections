b2047967ee22d980e93a08f92ee51fa6ad013ce7
crist <cristhianchulca49@gmail.com>
2025-11-19 13:12:04 +0100

    feat: implement game functions inside a while loop

--- src/main/java/Level1_E3/Main.java
+++ src/main/java/Level1_E3/Main.java
@@ -2,6 +2,10 @@ package Level1_E3;
 
 public class Main {
     public static void main(String[] args) {
-
+        do {
+            Game game = new Game();
+            game.initialGame(Input.readDocument("countries.txt"));
+            game.endOfGame();
+        } while (Input.readBoolean("Do you want start again? write: y (yes) o n (no)"));
     }
 }
